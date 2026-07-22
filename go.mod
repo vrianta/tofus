b8268a5fa90d2fcebe36cdac7a7ed59b2bd6ceed
tutoraddicts <rohitdutta907@gmail.com>
2026-07-22 23:53:06 +0530

    updated dependencies

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.24.3
 
 require (
 	github.com/vrianta/gonfig v0.0.0-20260703215428-3204eb22ab74
-	github.com/vrianta/gulog v1.0.0
+	github.com/vrianta/gulog v1.0.1
 )
--- vendor/github.com/vrianta/gulog/util.go
+++ vendor/github.com/vrianta/gulog/util.go
@@ -28,7 +28,7 @@ func init() {
 }
 
 func startLogging() {
-	// fmt.Println("gulog: startLogging goroutine started")
+	Info("gulog: Service has been started, make. \nIf you are using os.exit() or any unprecidented exit in the system please use gulog.Wait() in the main end")
 	waitG.Done()
 	for {
 		msg := <-massages
